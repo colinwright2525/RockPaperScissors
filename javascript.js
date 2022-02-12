ac9ef142fb70e3ad523e882761f3a9f41ed9b73b
Colin <colin.wright2525@gmail.com>
2022-02-12 18:58:53 -0500

    add javascript.js

--- javascript.js
+++ javascript.js
@@ -4,29 +4,56 @@ console.log('Hello World');
 
 const title = document.querySelector('div');
 const btn = document.querySelector('button');
-const res1 = document.querySelector('.result1');
-const res2 = document.querySelector('.result2');
-const res3 = document.querySelector('.result3');
-const res4 = document.querySelector('.result4');
-const res5 = document.querySelector('.result5');
+const res = document.querySelector('#result');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const human = document.querySelector('#human');
+const computer = document.querySelector('#computer');
+const reset = document.querySelector('#reset');
+
+
 
 console.log(title);
-console.log(res1);
+console.log(res);
 
 
 let playerSelection;
 let computerSelection;
 let result;
+let computerScore;
 let playerScore;
+let totalComputerScore = 0;
 let totalPlayerScore = 0;
 let gameVerdict;
 
-function playerPlay () {
-    playerSelection = prompt('Choose rock, paper, or scissors','');
+
+rock.addEventListener('click', () => {
+        playerSelection = 'rock';
+        playerSelection = playerSelection.toLowerCase();
+        game(playerSelection);
+    });
+
+paper.addEventListener('click', () => {
+    playerSelection = 'paper';
     playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
-    return playerSelection;
-}
+    game(playerSelection);
+});
+
+scissors.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    playerSelection = playerSelection.toLowerCase();
+    game(playerSelection);
+});
+
+reset.addEventListener('click', () => {
+    human.textContent = 'Human you(dumbass): ';
+    totalPlayerScore = 0;
+    computer.textContent = 'THE MACHINES: ';
+    totalComputerScore = 0;
+    title.textContent = 'The fight continues. Who will win this time, man or machine??';
+});
+
 
 function computerPlay () {
     let num = Math.floor(Math.random()*3) + 1;
@@ -52,51 +79,60 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === 'scissors') {
             result = 'You win, rock beats scissors!'
             playerScore = 1;
-            return {result, playerScore};
+            computerScore = 0;
+            return {result, playerScore, computerScore};
         }
         else if (computerSelection === 'paper') {
             result = 'You lose, paper beats rock!';
-            playerScore = -1;
-            return {result, playerScore};
+            playerScore = 0;
+            computerScore = 1;
+            return {result, playerScore, computerScore};
         }
         else {
             result = 'Tie! Play again!';
             playerScore = 0;
-            return {result, playerScore};
+            computerScore = 0;
+            return {result, playerScore, computerScore};
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             result = 'You win, paper beats rock!';
             playerScore = 1;
-            return {result, playerScore};
+            computerScore = 0;
+            return {result, playerScore, computerScore};
         }
         else if (computerSelection === 'scissors') {
             result = 'You lose, scissor beats paper!';
-            playerScore = -1;
-            return {result, playerScore};
+            playerScore = 0;
+            computerScore = 1;
+            return {result, playerScore, computerScore};
         }
         else {
             result = 'Tie! Play again!';
             playerScore = 0;
-            return {result, playerScore};
+            computerScore = 0;
+            return {result, playerScore, computerScore};
         }
     }
     else {
         if (computerSelection === 'paper') {
             result = 'You win, scissors beat paper!';
             playerScore = 1;
-            return {result, playerScore};
+            computerScore = 0;
+            return {result, playerScore, computerScore};
         }
         else if (computerSelection === 'rock') {
             result = 'You lose, rock beats scissors!';
-            playerScore = -1;
-            return {result, playerScore};
+            playerScore = 0;
+            computerScore = 1;
+            return {result, playerScore, computerScore};
         }
         else {
             result = 'Tie! Play again!';
             playerScore = 0;
-            return {result, playerScore};
+            computerScore = 0;
+            return {result, playerScore, computerScore};
         }
     }
 }
@@ -104,49 +140,41 @@ function playRound(playerSelection, computerSelection) {
 
 
 
-
-
-
-function game() {
-    for (let i = 0; i < 5 && totalPlayerScore !== 3 && totalPlayerScore !== -3; i++) {
-        playerPlay();
+function game(playerSelection) {
         console.log(playerSelection);
         computerPlay();
+        console.log(computerSelection);
         playRound(playerSelection,computerSelection);
       
      
         console.log(result);
         totalPlayerScore += playerScore;
+        totalComputerScore += computerScore;
         console.log(totalPlayerScore);
-        console.log(res1);
-
-
-        if (i === 0) {
-            res1.textContent += (result);
-        }
-        else if (i === 1) {
-            res2.textContent += (result);
-        }
-        else if (i === 2) {
-            res3.textContent += (result);
-        }
-        else if (i === 3) {
-            res4.textContent += (result);
-        }
-        else if (i === 4) {
-            res5.textContent += (result);
-        }
-
-    }
-    if (totalPlayerScore > 0) {
-        gameVerdict = 'Yay! You won!';
-    }
-    else {
-        gameVerdict = 'Oh no, you lost...';
-    }
+        console.log(totalComputerScore);
     
-    title.textContent = (gameVerdict + ' If you had fun, click the button to play again!');
-    btn.textContent = ('Play Again!');
+
+        res.textContent = result;
+        human.textContent = `Human you(dumbass): ${totalPlayerScore}`;
+        computer.textContent = `THE MACHINES: ${totalComputerScore}`;
+        
+     
+    
+        if (totalPlayerScore === 5) {
+            gameVerdict = 'Yay! You won!';
+            title.textContent = (gameVerdict + ' If you had fun, click the button to play again!');
+         
+
+        }
+
+        else if (totalComputerScore === 5) {
+            gameVerdict = 'Oh no, you lost...';
+            title.textContent = (gameVerdict + ' If you had fun, click the button to play again!');
+        
+
+        }
+    
+  
   
 }
 
